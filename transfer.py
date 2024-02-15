23acc919fa5bda6444438c8030e9f71382094813
pratikshamusale <142825200+pratikshamusale@users.noreply.github.com>
2024-02-15 21:29:01 +0530

    Add files via upload

---
+++ transfer.py
@@ -0,0 +1,50 @@
+#Break Statement
+#In Python, the break statement provides you with the opportunity to exit out of
+# a loop when an external condition is triggered.
+# You’ll put the break statement within the block of code under your loop statement,
+# usually after a conditional if statement.
+
+# number = 0
+#
+# for number in range(10):
+#     if number == 5:
+#         break    # break here
+#
+#     print('Number is ' + str(number))
+#
+# print('Out of loop')
+
+#Continue Statement
+# The continue statement gives you the option to skip over the part of
+# a loop where an external condition is triggered,
+# but to go on to complete the rest of the loop. That is,
+# the current iteration of the loop will be disrupted,
+# but the program will return to the top of the loop.
+
+# number = 0
+#
+# for number in range(10):
+#     if number == 5:
+#         continue    # continue here
+#
+#     print('Number is ' + str(number))
+#
+# print('Out of loop')
+
+#pass Statement
+#When an external condition is triggered,
+# the pass statement allows you to handle the condition without the
+# loop being impacted in any way; all of the code will continue to be read unless a
+# break or other statement occurs.
+
+
+# number = 0
+#
+# for number in range(10):
+#     if number == 5:
+#         pass    # pass here
+#
+#     print('Number is ' + str(number))
+#
+# print('Out of loop')
+#
